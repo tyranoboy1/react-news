f47de0d215600c75ac05005717c249f57faffa37
jihoon Han <gkwlgnd@naver.com>
2023-04-06 15:13:00 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<NewsPage />} />
+        <Route path="/:category" element={<NewsPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
